39ea066dc2dfdd10174df10d3cdd3c2d7c48c3bc
JuanPabloUni <jp.hernandezt1@uniandes.edu.co>
2024-03-07 10:14:14 -0500

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -3,13 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Formulario from './Formulario';
+import Timer from './Timer';
+import Password from './Password'
+import RickAndMorty from './RickAndMorty';
+import RickAndMorty2 from './RickAndMorty2';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
+
+root.render(<Formulario />);
+
+root.render(<Timer />);
+
+root.render(<Password />);
+
+root.render(<RickAndMorty />)
+
+root.render(<RickAndMorty2 />)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
